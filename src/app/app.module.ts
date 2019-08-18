856b88f349f9489591da3f6dfdd7b27390669c6a
Hasan Aburayyan <hasanaburayyan@hotmail.com>
2019-08-18 06:31:35 -0400

    Restructured Folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './app/navbar/navbar.component';
-import { HeaderComponent } from './app/header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 import {RouterModule, Routes} from '@angular/router';
-import { HomeComponent } from './app/home/home.component';
-import { AboutComponent } from './app/about/about.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {path: 'about', component: AboutComponent},
